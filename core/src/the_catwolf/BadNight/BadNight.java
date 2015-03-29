b35a3354d42828ab5de1e33896fadb2de760f743
dotteri_thecatwolf <teritriano@gmail.com>
2015-03-29 17:40:32 +0200

    Test commit

--- core/src/the_catwolf/BadNight/BadNight.java
+++ core/src/the_catwolf/BadNight/BadNight.java
@@ -45,7 +45,7 @@ public class BadNight extends Game{
 	static public BadNight badNight;
 	final private static int VERSION = 1;
 	final private static int FEATURES = 6;
-	final private static int BUGS_SOLVED = 0;
+	final private static int BUGS_SOLVED = A;
 		
 	AssetManager assets;
 	
